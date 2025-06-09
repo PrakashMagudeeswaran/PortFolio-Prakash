0b124f513e01a828eea9492662b7bf966b7c41c9
Prakash <157131108+PrakashMagudeeswaran@users.noreply.github.com>
2025-06-09 23:26:58 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -55,7 +55,7 @@ export const Banner = () => {
               <div className= "animate__animated1">
                 <h1>{`Hi! I'm Prakash`} </h1>
                   <h2 className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer","Full-Stack Dev","React Dev","Management Graduate" ]'><span className="wrap">{text}</span></h2>
-                  <p>Specializing in Project and Marketing Management, complemented by hands-on training as a Full Stack Developer. Proficient in both front-end and back-end development technologies, eager to apply technical skills in a professional environment.</p>
+                  <p>When data meets decision making,business finds its edge, Insights aren't discovered but designed. Driven by data, aligned with business outcomes</p>
               </div>
           </Col>
           <Col xs={12} md={6} xl={5}>
